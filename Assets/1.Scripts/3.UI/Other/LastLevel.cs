a92118e472834eb0c6851f93737289d47f843196
djnjbjb <djnjbjb@126.com>
2023-01-29 19:36:56 +0800

    整理文件和脚本

---
+++ Assets/1.Scripts/3.UI/Other/LastLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ludo.TwoHandsWar.UI.Other
+{
+    public class LastLevel : MonoBehaviour
+    {
+        public void ReturnToMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
